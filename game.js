1c48668f2360b0e48f86f0859902449fdc6952b4
Bela-Mina <belaandrosbela@example.com>
2026-09-23 23:57:29 +0300

    feat: implement rock paper scissors round logic

--- game.js
+++ game.js
@@ -1,17 +1,40 @@
 
-//step 1//
-const choices = ["rock", "paper", "scissors"]
+//step 1 - computer Choice//
+
+const choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
-
-    let randomChoices =  choices[Math.floor(Math.random() * 3)];
-
-    return randomChoices
-}
+    return choices[Math.floor(Math.random() * 3)];
+};
 getComputerChoice()
-
+// Human Choice //
 function getHumanChoice() {
-   let huChoices =  prompt("Play The Game  - Rock, Paper, Secissor-  ")
-    return  huChoices
-}
-console.log(getHumanChoice())
+    return prompt("Play The Game  - Rock, Paper, Secissor-  ")
+};
 
+
+ 
+function playRound(humanChoice, computerChoice) {
+   if(
+    (humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+    console.log(` Human wins! ${humanChoice} beats ${computerChoice}`)
+    // one Professtional way to use each winning team use bracket
+   }  else if (humanChoice === computerChoice) {
+    console.log(`${humanChoice} It's a tie! ${computerChoice}`)
+   } else {
+    console.log(`Computer wins! ${computerChoice} beats ${humanChoice}`)
+   }
+}
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+console.log(playRound(humanChoice, computerChoice))
+
+
+
+   //(
+   // (humanChoice === "rock" && computerChoice === "rock")||
+   // (humanChoice === "secissors" && computerChoice === "scissors")||
+   // (humanChoice === "paper" && computerChoice === "paper")
+   //) 
+   
+    // this is method is good but you can do with simple method computerChoice === humanChoice
